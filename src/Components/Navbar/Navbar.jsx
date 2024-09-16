20bac1b97708c17e18e2d6be48394d3950457b05
ahmed <ahmed.zaakan@gmail.com>
2024-09-16 10:28:15 +0300

    End nav

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,70 +1,67 @@
 import React from 'react';
 import { FaBars } from "react-icons/fa";
-import Logo from "../"
+import Logo from "../../assets/images/logo.png"
+import './Navbar.css'
 
 
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-md  fixed-top">
-        <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg  fixed-top">
+        <div className="container">
           
           <div className="logoicon">
             <img src={Logo} alt="logo" />
           </div>
           <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-expanded="false" aria-label="Toggle navigation">
-          <FaBars />
+          <FaBars size={30} />
           </button>
-          <div className="collapse navbar-collapse d-md-flex" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-md-0">
+          <div className="collapse navbar-collapse d-lg-flex" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link active" aria-current="page" href="#home">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
+                <a className="nav-link" href="#about">About Us</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                <a className="nav-link" href="#course">All Course</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#pages">Pages</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#blog">Blog</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">Contact</a>
               </li>
             </ul>
           </div>
-          <div className="offcanvas offcanvas-start d-md-none" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-            <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+          <div className="offcanvas offcanvas-start d-lg-none" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div className="offcanvas-header ms-auto">
               <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div className="offcanvas-body">
               <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="#">Action</a></li>
-                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-                </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#home">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#about">About Us</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#course">All Course</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#pages">Pages</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#blog">Blog</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">Contact</a>
+              </li>
               </ul>
             </div>
           </div>
